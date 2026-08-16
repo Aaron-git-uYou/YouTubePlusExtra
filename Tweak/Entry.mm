7047ce838b58d51172c97713ce578f29630d8318
ace. <110989057+epic0002@users.noreply.github.com>
2026-08-15 21:27:24 -0400

    Bump to 0.8.4

--- Tweak/Entry.mm
+++ Tweak/Entry.mm
@@ -9,7 +9,7 @@
 #define YTKACE_COMBINED_SABR 0
 #endif
 
-NSString * const YTKACEVersion = @"0.8.3";
+NSString * const YTKACEVersion = @"0.8.4";
 
 static void YTKACEInstallModules(void) {
     YTKACEInstallSideloadCompatibilityHooks();
